6dd9bb02ff88d55b270b7ea7ade8ce113d5d77cd
Marcus Miguel <marcusmigueell@gmail.com>
2025-02-12 21:48:05 -0300

    EnvConfigService unit tests

---
+++ src/shared/infrastructure/env-config/__tests__/unit/env-config.service.spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from "@nestjs/testing"
+import { EnvConfigModule } from "../../env-config.module"
+import { EnvConfigService } from "../../env-config.service"
+
+describe("EnvConfigService unit tests", () => {
+
+    let sut: EnvConfigService
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                EnvConfigModule.forRoot()
+            ],
+            providers: [
+                EnvConfigService
+            ]
+        }).compile()
+
+        sut = module.get<EnvConfigService>(EnvConfigService)
+    })
+
+    it("should return the variable NODE_ENV", () => {
+        expect(sut.getNodeEnv()).toBe("test")
+    })
+
+    it("should return the variable PORT", () => {
+        expect(sut.getAppPort()).toBe(8080)
+    })
+})
