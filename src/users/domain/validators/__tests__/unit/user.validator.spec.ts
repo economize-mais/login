369562e821e22d048656e47b021f5c83bbdf4dde
Marcus Miguel <marcusmigueell@gmail.com>
2025-03-05 17:36:23 -0300

    User entity validation tests with class validator - createdAt field

--- src/users/domain/validators/__tests__/unit/user.validator.spec.ts
+++ src/users/domain/validators/__tests__/unit/user.validator.spec.ts
@@ -116,4 +116,22 @@ describe("UserValidator unit tests", () => {
             ])
         })
     })
+
+    describe("CreatedAt field", () => {
+        it("Invalidation cases for createdAt field", () => {
+            let isValid = sut.validate({ ...UserDataBuilder({}), createdAt: 10 as any })
+
+            expect(isValid).toBeFalsy()
+            expect(sut.errors["createdAt"]).toStrictEqual([
+                "createdAt must be a Date instance"
+            ])
+
+            isValid = sut.validate({ ...UserDataBuilder({}), createdAt: "05-03-2025" as any })
+
+            expect(isValid).toBeFalsy()
+            expect(sut.errors["createdAt"]).toStrictEqual([
+                "createdAt must be a Date instance"
+            ])
+        })
+    })
 })
