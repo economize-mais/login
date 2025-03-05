f4ac065a73b33616b0119a6846a5895c5bd0a7fe
Marcus Miguel <marcusmigueell@gmail.com>
2025-03-05 18:00:33 -0300

    Integration tests validate password field on create user

--- src/users/domain/entities/__tests__/integration/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.spec.ts
@@ -31,5 +31,19 @@ describe("UserEntity integration tests", () => {
             props = { ...UserDataBuilder({}), email: 10 as any }
             expect(() => new UserEntity(props)).toThrow(EntityValidationError)
         })
+
+        it("Should throw an error when creation a user with invalid password", () => {
+            let props = { ...UserDataBuilder({}), password: null }
+            expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+
+            props = { ...UserDataBuilder({}), password: "" }
+            expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+
+            props = { ...UserDataBuilder({}), password: "a".repeat(101) }
+            expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+
+            props = { ...UserDataBuilder({}), password: 10 as any }
+            expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+        })
     })
 })
