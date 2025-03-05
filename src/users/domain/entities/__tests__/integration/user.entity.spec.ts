33662531d958504cdd03195d219323a8f14ec7e9
Marcus Miguel <marcusmigueell@gmail.com>
2025-03-05 17:58:59 -0300

    Integration tests validate email field on create user

--- src/users/domain/entities/__tests__/integration/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.spec.ts
@@ -1,6 +1,6 @@
+import { EntityValidationError } from "@/shared/domain/errors/validation-error"
 import { UserDataBuilder } from "@/users/domain/testing/helpers/user-data-builder"
 import { UserEntity } from "../../user.entity"
-import { EntityValidationError } from "@/shared/domain/errors/validation-error"
 
 describe("UserEntity integration tests", () => {
     describe("Constuctor method", () => {
@@ -12,6 +12,23 @@ describe("UserEntity integration tests", () => {
             expect(() => new UserEntity(props)).toThrow(EntityValidationError)
 
             props = { ...UserDataBuilder({}), name: "a".repeat(256) }
+            expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+
+            props = { ...UserDataBuilder({}), name: 10 as any }
+            expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+        })
+
+        it("Should throw an error when creation a user with invalid email", () => {
+            let props = { ...UserDataBuilder({}), email: null }
+            expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+
+            props = { ...UserDataBuilder({}), email: "" }
+            expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+
+            props = { ...UserDataBuilder({}), email: "a".repeat(256) }
+            expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+
+            props = { ...UserDataBuilder({}), email: 10 as any }
             expect(() => new UserEntity(props)).toThrow(EntityValidationError)
         })
     })
