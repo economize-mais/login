0bc20a59eff0387112a07fc7ca8e69c5fa2f2ee6
Marcus Miguel <marcusmigueell@gmail.com>
2025-03-05 17:49:50 -0300

    Adding entity validation error

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -7,11 +7,13 @@ describe("UserEntity unit tests", () => {
     let sut: UserEntity
 
     beforeEach(() => {
+        UserEntity.validate = jest.fn()
         props = UserDataBuilder({})
         sut = new UserEntity(props)
     })
 
     it("Constructor method", () => {
+        expect(UserEntity.validate).toHaveBeenCalled()
         expect(sut.props.name).toEqual(props.name)
         expect(sut.props.email).toEqual(props.email)
         expect(sut.props.password).toEqual(props.password)
@@ -52,11 +54,13 @@ describe("UserEntity unit tests", () => {
     })
 
     it("Should update name a user", () => {
+        expect(UserEntity.validate).toHaveBeenCalled()
         sut.updateName("other name")
         expect(sut.props.name).toEqual("other name")
     })
 
     it("Should update the password field", () => {
+        expect(UserEntity.validate).toHaveBeenCalled()
         sut.updatePassword("other password")
         expect(sut.props.password).toEqual("other password")
     })
