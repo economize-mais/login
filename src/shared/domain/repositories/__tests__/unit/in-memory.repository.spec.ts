7a080fe0515ae616ea247b15691ab2382e4e8b89
Marcus Miguel <marcusmigueell@gmail.com>
2025-03-06 15:29:49 -0300

    update test name: input

--- src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
@@ -19,7 +19,7 @@ describe("InMemoryRepository unit tests", () => {
     })
 
     it("should save an entity", async () => {
-        const entity = new StubEntity({ name: "product", price: 10 })
+        const entity = new StubEntity({ name: "test name", price: 10 })
         await sut.save(entity)
         expect(entity.toJSON()).toStrictEqual(sut.items[0].toJSON())
     })
@@ -29,28 +29,28 @@ describe("InMemoryRepository unit tests", () => {
     })
 
     it("should get a entity by id", async () => {
-        const entity = new StubEntity({ name: "product", price: 10 })
+        const entity = new StubEntity({ name: "test name", price: 10 })
         await sut.save(entity)
         const result = await sut.getById(entity.id)
         expect(entity.toJSON()).toStrictEqual(result.toJSON())
     })
 
     it("should returns all entities", async () => {
-        const entity = new StubEntity({ name: "product", price: 10 })
+        const entity = new StubEntity({ name: "test name", price: 10 })
         await sut.save(entity)
         const result = await sut.getAll()
         expect([entity]).toStrictEqual(result)
     })
 
     it("should throw error on update when entity not found", async () => {
-        const entity = new StubEntity({ name: "product", price: 10 })
+        const entity = new StubEntity({ name: "test name", price: 10 })
         await expect(sut.update(entity)).rejects.toThrow(new NotFoundError("Entity not found"))
     })
 
     it("should update an entity", async () => {
-        const entity = new StubEntity({ name: "product", price: 10 })
+        const entity = new StubEntity({ name: "test name", price: 10 })
         await sut.save(entity)
-        const entityUpdated = new StubEntity({ name: "product", price: 20 }, entity.id)
+        const entityUpdated = new StubEntity({ name: "test name", price: 20 }, entity.id)
         await sut.update(entityUpdated)
         expect(entityUpdated.toJSON()).toStrictEqual(sut.items[0].toJSON())
     })
@@ -60,7 +60,7 @@ describe("InMemoryRepository unit tests", () => {
     })
 
     it("should delete an entity", async () => {
-        const entity = new StubEntity({ name: "product", price: 10 })
+        const entity = new StubEntity({ name: "test name", price: 10 })
         await sut.save(entity)
         await sut.delete(entity.id)
         expect(sut.items).toHaveLength(0)
