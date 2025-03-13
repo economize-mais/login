78508fc747e994c40696de63855549005e6f4f15
Marcus Miguel <marcusmigueell@gmail.com>
2025-03-12 23:18:52 -0300

    adding search method inMemory part 1

--- src/shared/domain/repositories/in-memory-searchable.repository.ts
+++ src/shared/domain/repositories/in-memory-searchable.repository.ts
@@ -1,15 +1,27 @@
 import { Entity } from "../entities/entity"
 import { InMemoryRepository } from "./in-memory.repository"
-import { SearchableRepositoryInterface } from "./searchable-repository-contracts"
+import { SearchableRepositoryInterface, SearchParams, SearcResult } from "./searchable-repository-contracts"
 
 export abstract class InMemorySearchableRepository<E extends Entity<Props>, Props> extends InMemoryRepository<E, Props> implements SearchableRepositoryInterface<E, Props, any, any> {
-    async search(props: any): Promise<E[]> {
-        const items = this.items.filter(item => {
-            return Object.keys(props).every(key => {
-                return item.props[key] === props[key];
-            });
-        });
-        return items;
+    async search(props: SearchParams): Promise<SearcResult<E>> {
+
     }
 
+    protected abstract applyFilter(item: E[], filter: string | null): Promise<E[]>
+
+    protected async applySort(
+        items: E[],
+        sort: string | null,
+        sortDir: string | null
+    ): Promise<E[]> {
+
+    }
+
+    protected async applyPaginated(
+        items: E[],
+        page: SearchParams["page"],
+        perPage: SearchParams["perPage"]
+    ): Promise<E[]> {
+
+    }
 }
