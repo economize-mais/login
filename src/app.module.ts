2c09e1227ffd3408f8f8ce19f87f8884840cff58
Marcus Miguel <marcusmigueell@gmail.com>
2025-03-26 23:40:41 -0300

    Adding prisma config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from "@nestjs/common"
 import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
+import { DatabaseModule } from './shared/infrastructure/database/database.module'
 import { EnvConfigModule } from "./shared/infrastructure/env-config/env-config.module"
 import { UsersModule } from "./users/infrastructure/users.module"
 
 @Module({
     imports: [
         EnvConfigModule,
-        UsersModule
+        UsersModule,
+        DatabaseModule
     ],
     controllers: [
         AppController
