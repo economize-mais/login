9620ccf35e7473894aded316d94a9f7b5860d05d
Marcus Miguel <marcusmigueell@gmail.com>
2025-03-31 23:40:16 -0300

    UserPrismaRepository integration tests

---
+++ src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma-repository.int-spec.ts
@@ -0,0 +1,41 @@
+import { DatabaseModule } from "@/shared/infrastructure/database/database.module"
+import { NotFoundError } from "@/shared/domain/errors/not-found-error"
+import { PrismaClient } from "@prisma/client"
+import { setupPrismaTests } from "@/shared/infrastructure/database/prisma/testing/setup-prisma-tests"
+import { Test, TestingModule } from "@nestjs/testing"
+import { UserDataBuilder } from "@/users/domain/testing/helpers/user-data-builder"
+import { UserEntity } from "@/users/domain/entities/user.entity"
+import { UserPrismaRepository } from "../../user-prisma.repository"
+
+describe("UserPrismaRepository integration tests", () => {
+    const prismaService =  new PrismaClient()
+    let sut: UserPrismaRepository
+    let module: TestingModule
+
+    beforeAll(async () => {
+        setupPrismaTests()
+        module = await Test.createTestingModule({
+            imports: [DatabaseModule.forTest(prismaService)]
+        }).compile()
+    })
+
+    beforeEach(async () => {
+        sut = new UserPrismaRepository(prismaService as any)
+        await prismaService.user.deleteMany()
+    })
+
+    it("should throws error when entity not found", async () => {
+        expect(() => sut.getById("fakeId")).rejects.toThrow(
+            new NotFoundError("UserModel not found usind ID fakeId")
+        )
+    })
+
+    it("should finds a entity by id", async () => {
+        const entity = new UserEntity(UserDataBuilder({}))
+        const newUser = await prismaService.user.create({
+            data: entity.toJSON()
+        })
+        const output = await sut.getById(newUser.id)
+        expect(output.toJSON()).toStrictEqual(entity.toJSON())
+    })
+})
