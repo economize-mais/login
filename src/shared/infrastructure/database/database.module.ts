4066aafe58df1aa900f1aa50b82f9a826736dc37
Marcus Miguel <marcusmigueell@gmail.com>
2025-03-31 23:25:27 -0300

    Adding forTest static method

--- src/shared/infrastructure/database/database.module.ts
+++ src/shared/infrastructure/database/database.module.ts
@@ -1,8 +1,10 @@
-import { Module } from "@nestjs/common"
 import { ConfigService } from "@nestjs/config"
+import { DynamicModule, Global, Module } from "@nestjs/common"
 import { EnvConfigModule } from "../env-config/env-config.module"
+import { PrismaClient } from "@prisma/client"
 import { PrismaService } from "./prisma/prisma.service"
 
+@Global()
 @Module({
     imports: [
         EnvConfigModule.forRoot()
@@ -16,4 +18,16 @@ import { PrismaService } from "./prisma/prisma.service"
     ]
 })
 
-export class DatabaseModule {}
+export class DatabaseModule {
+    static forTest(prismaClient: PrismaClient): DynamicModule {
+        return {
+            module: DatabaseModule,
+            providers: [
+                {
+                    provide: PrismaService,
+                    useFactory: () => prismaClient as PrismaService
+                }
+            ]
+        }
+    }
+}
