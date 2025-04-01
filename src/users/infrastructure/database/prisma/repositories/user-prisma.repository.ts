2bed517c3bb6158656117ac6c2d621147aa48372
Marcus Miguel <marcusmigueell@gmail.com>
2025-03-31 22:58:23 -0300

    UserPrismaRepository - implementing getById

--- src/users/infrastructure/database/prisma/repositories/user-prisma.repository.ts
+++ src/users/infrastructure/database/prisma/repositories/user-prisma.repository.ts
@@ -1,6 +1,8 @@
-import { PrismaService } from "@/shared/infrastructure/database/prisma/prisma.service";
-import { UserEntity } from "@/users/domain/entities/user.entity";
-import { UserRepository } from "@/users/domain/repositories/user.repository";
+import { NotFoundError } from "@/shared/domain/errors/not-found-error"
+import { PrismaService } from "@/shared/infrastructure/database/prisma/prisma.service"
+import { UserEntity } from "@/users/domain/entities/user.entity"
+import { UserModelMapper } from "../../models/user-model.mapper"
+import { UserRepository } from "@/users/domain/repositories/user.repository"
 
 export class UserPrismaRepository implements UserRepository.Repository {
 
@@ -18,6 +20,10 @@ export class UserPrismaRepository implements UserRepository.Repository {
         throw new Error("Method not implemented.");
     }
 
+    getById(id: string): Promise<UserEntity> {
+        return this._get(id)
+    }
+
     search(props: UserRepository.SearchParams): Promise<UserRepository.SearcResult> {
         throw new Error("Method not implemented.");
     }
@@ -29,15 +35,24 @@ export class UserPrismaRepository implements UserRepository.Repository {
         throw new Error("Method not implemented.");
     }
 
-    getById(id: string): Promise<UserEntity> {
-        throw new Error("Method not implemented.");
-    }
-
     save(entity: UserEntity): Promise<void> {
         throw new Error("Method not implemented.");
     }
 
     update(entity: UserEntity): Promise<void> {
         throw new Error("Method not implemented.");
+    }
+
+    protected async _get(id: string): Promise<UserEntity> {
+        try {
+            const user = await this.prismaService.user.findUnique({
+                where: { id }
+            })
+
+            return UserModelMapper.toEntity(user)
+
+        } catch (err) {
+            throw new NotFoundError(`UserModel not found usind ID ${id}`)
+        }
     }
 }
