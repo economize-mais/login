6eb01882a0d4ad043c4d28e1c266761077e6c8f2
Marcus Miguel <marcusmigueell@gmail.com>
2025-04-01 22:54:13 -0300

    UserPrismaRepository - implementing search method

--- src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma-repository.int-spec.ts
+++ src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma-repository.int-spec.ts
@@ -52,7 +52,7 @@ describe("UserPrismaRepository integration tests", () => {
 
     it("should returns all users", async () => {
         const entity = new UserEntity(UserDataBuilder({}))
-        const newUser = await prismaService.user.create({
+        await prismaService.user.create({
             data: entity.toJSON()
         })
         const entities = await sut.getAll()
--- src/users/infrastructure/database/prisma/repositories/user-prisma.repository.ts
+++ src/users/infrastructure/database/prisma/repositories/user-prisma.repository.ts
@@ -6,7 +6,10 @@ import { UserRepository } from "@/users/domain/repositories/user.repository"
 
 export class UserPrismaRepository implements UserRepository.Repository {
 
-    sortableFields: string[];
+    sortableFields: string[] = [
+        "name",
+        "createdAt"
+    ]
 
     constructor(
         private prismaService: PrismaService
@@ -24,8 +27,49 @@ export class UserPrismaRepository implements UserRepository.Repository {
         return this._get(id)
     }
 
-    search(props: UserRepository.SearchParams): Promise<UserRepository.SearcResult> {
-        throw new Error("Method not implemented.");
+    async search(props: UserRepository.SearchParams): Promise<UserRepository.SearcResult> {
+        const sortable = this.sortableFields?.includes(props.sort.by) || false
+        const orderByField = sortable ? props.sort.by : "createdAt"
+        const orderByDir = sortable ? props.sort.direction : "desc"
+
+        const count = await this.prismaService.user.count({
+            ...(props.filter && {
+                where: {
+                    name: {
+                        contains: props.filter,
+                        mode: "insensitive"
+                    }
+                }
+            })
+        })
+
+        const models = await this.prismaService.user.findMany({
+            ...(props.filter && {
+                where: {
+                    name: {
+                        contains: props.filter,
+                        mode: "insensitive"
+                    }
+                },
+                orderBy: {
+                    [orderByField]: orderByDir
+                },
+                skip: props.page && props.page > 0 ? (props.page - 1) * props.perPage : 1,
+                take: props.perPage && props.perPage > 0 ? props.perPage : 15
+            })
+        })
+
+        return new UserRepository.SearcResult({
+            items: models.map(model => UserModelMapper.toEntity(model)),
+            total: count,
+            currentPage: props.page,
+            perPage: props.perPage,
+            sort: {
+                by: orderByField,
+                direction: orderByDir
+            },
+            filter: props.filter
+        })
     }
 
     delete(id: string): Promise<void> {
