79ce68327021b40854a2e0e00354257cba4efe14
Marcus Miguel <marcusmigueell@gmail.com>
2025-04-08 00:23:59 -0300

    UsersCollectionPresenter unit tests

--- src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
+++ src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
@@ -1,5 +1,6 @@
 import { instanceToPlain } from "class-transformer"
-import { UserPresenter } from "../../user.presenter"
+import { UserCollectionPresenter, UserPresenter } from "../../user.presenter"
+import { PaginationPresenter } from "@/shared/infrastructure/presenters/pagination.presenter"
 
 describe("UsersPresenter unit tests", () => {
 
@@ -34,6 +35,70 @@ describe("UsersPresenter unit tests", () => {
             name: "test name",
             email: "a@a.com",
             createdAt: createdAt.toISOString()
+        })
+    })
+})
+
+describe("UsersCollectionPresenter unit tests", () => {
+
+    const createdAt = new Date()
+
+    let props = {
+        id: "d42ccf19-b246-4d1e-b64e-c3bdc392bd3f",
+        name: "test name",
+        email: "a@a.com",
+        password: "fake",
+        createdAt
+    }
+
+    describe("constuctor", () => {
+        it("should set values", () => {
+            const sut = new UserCollectionPresenter({
+                items: [props],
+                currentPage: 1,
+                perPage: 2,
+                lastPage: 1,
+                total: 1
+            })
+            expect(sut.meta).toBeInstanceOf(PaginationPresenter)
+            expect(sut.meta).toStrictEqual(
+                new PaginationPresenter({
+                    currentPage: 1,
+                    perPage: 2,
+                    lastPage: 1,
+                    total: 1
+                })
+            )
+            expect(sut.data).toStrictEqual(
+                [new UserPresenter(props)]
+            )
+        })
+    })
+
+    it("should presenter data", () => {
+        const sut = new UserCollectionPresenter({
+            items: [props],
+            currentPage: 1,
+            perPage: 2,
+            lastPage: 1,
+            total: 1
+        })
+        const output = instanceToPlain(sut)
+        expect(output).toStrictEqual({
+            data: [
+                {
+                    id: "d42ccf19-b246-4d1e-b64e-c3bdc392bd3f",
+                    name: "test name",
+                    email: "a@a.com",
+                    createdAt: createdAt.toISOString()
+                }
+            ],
+            meta: {
+                currentPage: 1,
+                perPage: 2,
+                lastPage: 1,
+                total: 1
+            }
         })
     })
 })
