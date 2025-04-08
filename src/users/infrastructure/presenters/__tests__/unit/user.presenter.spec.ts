630fa253fcd5b310ffd5b9bcdc1c14f6ab9a864f
Marcus Miguel <marcusmigueell@gmail.com>
2025-04-07 23:39:54 -0300

    UsersPresenter unit tests - part 2

--- src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
+++ src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
@@ -1,8 +1,10 @@
+import { instanceToPlain } from "class-transformer"
 import { UserPresenter } from "../../user.presenter"
 
 describe("UsersPresenter unit tests", () => {
 
     const createdAt = new Date()
+    let sut: UserPresenter
 
     let props = {
         id: "d42ccf19-b246-4d1e-b64e-c3bdc392bd3f",
@@ -12,13 +14,26 @@ describe("UsersPresenter unit tests", () => {
         createdAt
     }
 
+    beforeEach(() => {
+        sut = new UserPresenter(props)
+    })
+
     describe("constuctor", () => {
         it("should be defined", () => {
-            const sut = new UserPresenter(props)
             expect(sut.id).toEqual(props.id)
             expect(sut.name).toEqual(props.name)
             expect(sut.email).toEqual(props.email)
             expect(sut.createdAt).toEqual(props.createdAt)
+        })
+    })
+
+    it("should presenter data", () => {
+        const output = instanceToPlain(sut)
+        expect(output).toStrictEqual({
+            id: "d42ccf19-b246-4d1e-b64e-c3bdc392bd3f",
+            name: "test name",
+            email: "a@a.com",
+            createdAt: createdAt.toISOString()
         })
     })
 })
