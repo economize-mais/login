ef0f72ccdd1897ddb67bc47f15143f9c84bc6be5
Marcus Miguel <marcusmigueell@gmail.com>
2025-04-07 21:45:52 -0300

    ListUserUseCase integration tests - part 1

---
+++ src/users/application/usecases/__tests__/integration/list-user.usecase.int-spec.ts
@@ -0,0 +1,64 @@
+import { PrismaClient } from "@prisma/client"
+import { Test, TestingModule } from "@nestjs/testing"
+import { DatabaseModule } from "@/shared/infrastructure/database/database.module"
+import { setupPrismaTests } from "@/shared/infrastructure/database/prisma/testing/setup-prisma-tests"
+import { UserPrismaRepository } from "@/users/infrastructure/database/prisma/repositories/user-prisma.repository"
+import { UserEntity } from "@/users/domain/entities/user.entity"
+import { UserDataBuilder } from "@/users/domain/testing/helpers/user-data-builder"
+import { ListUserUseCase } from "../../list-user.usecase"
+
+describe("ListUserUseCase integration tests", () => {
+    const prismaService =  new PrismaClient()
+    let sut: ListUserUseCase.UseCase
+    let repository: UserPrismaRepository
+    let module: TestingModule
+
+    beforeAll(async () => {
+        setupPrismaTests()
+        module = await Test.createTestingModule({
+            imports: [DatabaseModule.forTest(prismaService)]
+        }).compile()
+
+        repository = new UserPrismaRepository(prismaService as any)
+    })
+
+    beforeEach(async () => {
+        sut = new ListUserUseCase.UseCase(repository)
+        await prismaService.user.deleteMany()
+    })
+
+    afterAll(async () => {
+        await module.close()
+    })
+
+    it("should returns the users ordered by createdAt", async () => {
+        const createdAt = new Date()
+        const entities: UserEntity[] = []
+
+        const arrange = Array(3).fill(UserDataBuilder({}))
+
+        arrange.forEach((element, index) => {
+            entities.push(
+                new UserEntity({
+                    ...element,
+                    email: `tests${index}@mail.com`,
+                    createdAt: new Date(createdAt.getTime() + index)
+                })
+            )
+        })
+
+        await prismaService.user.createMany({
+            data: entities.map(item => item.toJSON())
+        })
+
+        const output = await sut.execute({})
+
+        expect(output).toStrictEqual({
+            items: entities.reverse().map(item => item.toJSON()),
+            total: 3,
+            currentPage: 1,
+            perPage: 15,
+            lastPage: 1
+        })
+    })
+})
