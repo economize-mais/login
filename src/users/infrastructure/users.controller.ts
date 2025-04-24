6fddde162e0ff85a2b25e40d82ed5360386cfdfa
Marcus Miguel <marcusmigueell@gmail.com>
2025-04-24 16:04:12 -0300

    Documentation update method

--- src/users/infrastructure/users.controller.ts
+++ src/users/infrastructure/users.controller.ts
@@ -179,6 +179,19 @@ export class UsersController {
         return UsersController.userToResponse(output)
     }
 
+    @ApiBearerAuth()
+    @ApiResponse({
+        status: 404,
+        description: "Id não encontrado"
+    })
+    @ApiResponse({
+        status: 401,
+        description: "Acesso não autorizado"
+    })
+    @ApiResponse({
+        status: 422,
+        description: "Corpo da requisição com dados inválidos"
+    })
     @UseGuards(AuthGuard)
     @Put(":id")
     async update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
