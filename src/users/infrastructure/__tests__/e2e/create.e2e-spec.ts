024b99e38270aaf673aa2ceefc3c9afd9ff37856
Marcus Miguel <marcusmigueell@gmail.com>
2025-04-23 21:44:00 -0300

    UsersController e2e tests - create method

---
+++ src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
@@ -0,0 +1,71 @@
+import request from "supertest"
+import { Test, TestingModule } from "@nestjs/testing"
+
+import { EnvConfigModule } from "@/shared/infrastructure/env-config/env-config.module"
+import { DatabaseModule } from "@/shared/infrastructure/database/database.module"
+import { INestApplication } from "@nestjs/common"
+import { instanceToPlain } from "class-transformer"
+import { PrismaClient } from "@prisma/client"
+import { setupPrismaTests } from "@/shared/infrastructure/database/prisma/testing/setup-prisma-tests"
+import { SignupDto } from "../../dtos/signup.dto"
+import { UserRepository } from "@/users/domain/repositories/user.repository"
+import { UsersModule } from "../../users.module"
+import { UsersController } from "../../users.controller"
+
+describe("UsersController unit tests", () => {
+
+    let app: INestApplication
+    let module: TestingModule
+    let repo: UserRepository.Repository
+    let signupDto: SignupDto
+
+    const prismaService = new PrismaClient()
+
+    beforeAll(async () => {
+        setupPrismaTests()
+
+        module = await Test.createTestingModule({
+            imports: [
+                EnvConfigModule,
+                UsersModule,
+                DatabaseModule.forTest(prismaService)
+            ]
+        }).compile()
+
+        app = module.createNestApplication()
+        await app.init()
+        repo = module.get<UserRepository.Repository>("UserRepository")
+    })
+
+    beforeEach(async () => {
+        signupDto = {
+            name: "test name",
+            email: "a@a.com",
+            password: "TestPassword123"
+        }
+
+        await prismaService.user.deleteMany()
+    })
+
+    describe("POST /users", () => {
+        it("should create a user", async () => {
+            const res = await request(app.getHttpServer())
+                .post("/users")
+                .send(signupDto)
+                .expect(201)
+
+            expect(Object.keys(res.body)).toStrictEqual([
+                "id",
+                "name",
+                "email",
+                "createdAt"
+            ])
+
+            const user = await repo.getById(res.body.id)
+            const presenter = UsersController.userToResponse(user.toJSON())
+            const serialized = instanceToPlain(presenter)
+
+            expect(res.body).toStrictEqual(serialized)
+        })
+    })
+})
