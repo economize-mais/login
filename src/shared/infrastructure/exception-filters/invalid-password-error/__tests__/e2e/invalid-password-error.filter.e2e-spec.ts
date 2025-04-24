33c85a7e7a27fc5db73281f2fcf1cc8f2a9d32b6
Marcus Miguel <marcusmigueell@gmail.com>
2025-04-24 01:08:02 -0300

    InvalidPasswordErrorFilter (e2e) test

--- src/shared/infrastructure/exception-filters/invalid-password-error/__tests__/e2e/invalid-password-error.filter.e2e-spec.ts
+++ src/shared/infrastructure/exception-filters/invalid-password-error/__tests__/e2e/invalid-password-error.filter.e2e-spec.ts
@@ -1,7 +1,45 @@
+import { Controller, INestApplication, Put } from "@nestjs/common"
 import { InvalidPasswordErrorFilter } from "../../invalid-password-error.filter"
+import { InvalidPasswordError } from "@/shared/application/errors/invalid-password-error"
+import { Test, TestingModule } from "@nestjs/testing"
+import request from "supertest"
+
+@Controller("stub")
+class StubController {
+    @Put()
+    index() {
+        throw new InvalidPasswordError("Old password does not match")
+    }
+}
 
 describe("InvalidPasswordErrorFilter (e2e)", () => {
+    let app: INestApplication
+    let module: TestingModule
+
+    beforeAll(async () => {
+        module = await Test.createTestingModule({
+            controllers: [
+                StubController
+            ]
+        }).compile()
+        app = module.createNestApplication()
+        app.useGlobalFilters(
+            new InvalidPasswordErrorFilter()
+        )
+        await app.init()
+    })
+
     it("should be defined", () => {
         expect(new InvalidPasswordErrorFilter()).toBeDefined()
+    })
+
+    it("should catch a InvalidPasswordError", () => {
+        return request(app.getHttpServer()).put("/stub")
+            .expect(422)
+            .expect({
+                statusCode: 422,
+                error: "Unprocessable Entity",
+                message: "Old password does not match"
+            })
     })
 })
