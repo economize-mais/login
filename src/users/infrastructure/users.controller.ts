5e56cc8f7d8914fd77e822e2f3d489e2ff3e050c
Marcus Miguel <marcusmigueell@gmail.com>
2025-04-24 16:00:40 -0300

    Documentation response login

--- src/users/infrastructure/users.controller.ts
+++ src/users/infrastructure/users.controller.ts
@@ -86,6 +86,29 @@ export class UsersController {
         return UsersController.userToResponse(output)
     }
 
+    @ApiResponse({
+        status: 200,
+        schema: {
+            type: "object",
+            properties: {
+                accessToken: {
+                    type: "string"
+                }
+            }
+        }
+    })
+    @ApiResponse({
+        status: 422,
+        description: "Parametros de consulta inválidos"
+    })
+    @ApiResponse({
+        status: 404,
+        description: "E-mail não encontrado"
+    })
+    @ApiResponse({
+        status: 400,
+        description: "Credenciais inválidas"
+    })
     @HttpCode(200)
     @Post("login")
     async login(@Body() signinDto: SigninDto) {
